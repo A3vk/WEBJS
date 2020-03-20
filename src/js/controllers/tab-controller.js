5bc37f67369635722842016a1a2e47ed70463229
Kevin Treffers <kevintreffers@msn.com>
2020-03-20 23:17:10 +0100

    Finished Tabs
    tab view and tab controller are both finished.
    The tab controller has a variable currentTab that
    can be used to see what tab the screen is currently on.

--- src/js/controllers/tab-controller.js
+++ src/js/controllers/tab-controller.js
@@ -1,7 +1,20 @@
 import TabView from '../views/tabs/tab-view';
+import WarehousController from './warehouse-controller';
 
 export default class TabController{
-    constructor(){
+    constructor(warehousController){
+        this.types = [];
+        
+        for (var key in warehousController.warehouses){
+            this.types.push(warehousController.warehouses[key].type);
+        }
+        this.currentType = this.types[1];
         this.tabView = new TabView();
+        this.tabView.switchTab = (data) => {
+            this.currentTab = data;
+        };
+        this.tabView.render(this.types);
+        this.tabView.switchTab(this.types[0]);
+        console.dir(this.currentTab);
     }
 }
--- src/js/views/tabs/tab-view.js
+++ src/js/views/tabs/tab-view.js
@@ -1,15 +1,54 @@
 export default class TabView{
-    constructor(){
+    render(types){
         this.tabList = document.querySelector('.tab-list')
 
-        this.li = document.createElement('li');
-        this.li.className = 'nav-item';
-        this.button = document.createElement('button');
-        this.text = document.createTextNode('Kleding');
-        this.button.appendChild(this.text);
-        this.button.className = 'btn-left';
-        this.li.append(this.button);
+        for (let i in types){
 
-        this.tabList.append(this.li);
+            this.li = document.createElement('li');
+            this.li.className = 'nav-item';
+
+            this.button = document.createElement('button');
+            this.button.id = types[i];
+
+            switch (types[i]) {
+                case 'clothing':
+                    this.text = document.createTextNode('Kleding');
+                    break;
+
+                case 'decoration':
+                    this.text = document.createTextNode('Decoratie');
+                    break;
+
+                case 'beautification':
+                    this.text = document.createTextNode('Tierelantijn');
+                    break;
+
+                default:
+                    break;
+            }
+
+            this.button.appendChild(this.text);
+            this.li.append(this.button);
+            this.tabList.append(this.li);
+
+            this.max = i;
+        }
+
+        this.tabs = document.querySelectorAll('.tab-list > li > button')
+
+        this.tabs[0].className = 'active btn-left';
+        this.tabs[this.max].className = 'btn-right';
+        
+        for (let i = 0; i < this.tabs.length; i++) {
+            this.tabs[i].addEventListener('click', () => {
+                this.tab = document.querySelector('button.active');
+                this.tab.classList.remove('active')
+
+                this.tabs[i].classList.add('active');
+
+                this.switchTab(this.tabs[i].id);
+            });
+            
+        }
     }
 }
