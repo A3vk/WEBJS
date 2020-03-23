652d00b906b4f9ab09376093be17b02c5eaf4524
Kevin Treffers <kevintreffers@msn.com>
2020-03-23 23:46:39 +0100

    Fixed

--- src/js/helpers/storage-helper.js
+++ src/js/helpers/storage-helper.js
@@ -57,7 +57,32 @@ export default class StorageHelper {
 				]
 			},
 			decoration: {
-				products: [],
+				products: [
+					{
+						id: 1,
+						type: "decoration",
+						name: "Grafsteen",
+						description: "Sppoookkkyyyyy!!!",
+						purchasePrice: 10,
+						sellingPrice: 20,
+						minamalStock: 50,
+						stock: 36,
+						weight: 250,
+						image: 'https://www.colorbook.io/imagecreator.php?width=55&height=55?'
+					},
+					{
+						id: 2,
+						type: "decoration",
+						name: "Tuinkabouter",
+						description: "Ik heb een punt muts",
+						purchasePrice: 999,
+						sellingPrice: 1,
+						minamalStock: 5,
+						stock: 5,
+						weight: 2,
+						image: 'https://www.colorbook.io/imagecreator.php?width=55&height=55?'
+					}
+				],
 				warehouse: [
 					[ 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0 ],
 					[ 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0, -1, 0 ],
@@ -77,7 +102,36 @@ export default class StorageHelper {
 				]
 			},
 			beautification: {
-				products: [],
+				products: [
+					{
+						id: 1,
+						type: "beautification",
+						name: "Carl",
+						description: "erg hongerig",
+						purchasePrice: 37,
+						sellingPrice: 37.37,
+						minamalStock: 1,
+						stock: 1,
+						weight: '225 X 90 X 119',
+						color: "#ff0000",
+						pieces: "2",
+						image: 'https://www.colorbook.io/imagecreator.php?width=55&height=55?'
+					},
+					{
+						id: 2,
+						type: "beautification",
+						name: "1-UP",
+						description: "Voor mensen die meer dan 1 leven nodig hebben",
+						purchasePrice: 1,
+						sellingPrice: 999999,
+						minamalStock: 10,
+						stock: 8,
+						weight: '225 X 90 X 119',
+						color: "#ff0000",
+						pieces: "100",
+						image: 'https://www.colorbook.io/imagecreator.php?width=55&height=55?'
+					}
+				],
 				warehouse: [
 					[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
 					[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
